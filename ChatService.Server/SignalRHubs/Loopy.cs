4c5cd38ad0e7ad818e5856b179f14dcc855f72ae
Martin <Martin@DESKTOP-FKIDL0O>
2020-03-05 17:45:36 +0200

    Add project files.

---
+++ ChatService.Server/SignalRHubs/Loopy.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatService.Server.SignalRHubs
+{
+    public class Loopy : Hub
+    {
+        public Task Send(string data)
+        {
+            return Clients.All.SendAsync("Send", data);
+        }
+    }
+}
---
+++ ChatService.Server/Startup.cs
@@ -0,0 +1,58 @@
+using ChatService.Server.SignalRHubs;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ChatService.Server
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddCors(setup =>
+            {
+                setup.AddPolicy("AllowAllRequests", policy =>
+                {
+                    policy
+                    .WithOrigins("http://localhost:50963")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials();
+                });
+            });
+            services.AddSignalR();
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors("AllowAllRequests");
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapHub<Loopy>("/loopy");
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
